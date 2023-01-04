99de1743fd633dad030b129a58c648c13de96de9
hkumbh1 <121787611+hkumbh1@users.noreply.github.com>
2023-01-04 19:22:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,6 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-2"
-}
-
-variable "key_name" {
-  description = " SSH keys to connect to ec2 instance"
-  default     =  "Demo-Web"
+  default     = "ap-south-1"
 }
 
 variable "instance_type" {
